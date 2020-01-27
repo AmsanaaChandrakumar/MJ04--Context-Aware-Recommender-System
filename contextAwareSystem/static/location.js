cb85621697b471d3189c006083e5be8ecb30d7a2
Jas Lamba <jaskiran.lamba@gmail.com>
2020-01-27 14:22:26 -0500

    adding in automatic location

--- contextAwareSystem/static/location.js
+++ contextAwareSystem/static/location.js
@@ -1,22 +1,55 @@
+var geocoder;
+
 if (navigator.geolocation) {
-    var location_timeout = setTimeout("geolocFail()", 20000);
-
-    navigator.geolocation.getCurrentPosition(function(position) {
-        clearTimeout(location_timeout);
-
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
-
-        console.log(lat, lng);
-    }, function(error) {
-        clearTimeout(location_timeout);
-        geolocFail();
-    });
-} else {
-    // Fallback for no geolocation
-    geolocFail();
+	navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+} 
+//Get the latitude and the longitude;
+function successFunction(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    codeLatLng(lat, lng)
 }
 
-function geolocFail() {
-	console.log("failed");
+function errorFunction(){
+	alert("Geocoder failed");
 }
+
+function initialize() {
+	geocoder = new google.maps.Geocoder();
+	console.log("in tuna2")
+}
+
+function codeLatLng(lat, lng) {
+
+var latlng = new google.maps.LatLng(lat, lng);
+geocoder.geocode({'latLng': latlng}, function(results, status) {
+  if (status == google.maps.GeocoderStatus.OK) {
+  console.log(results)
+    if (results[1]) {
+     //formatted address
+     alert(results[0].formatted_address)
+    //find country name
+         for (var i=0; i<results[0].address_components.length; i++) {
+        for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+        //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+            if (results[0].address_components[i].types[b] == "locality") {
+                //this is the object you are looking for
+                city= results[0].address_components[i];
+                break;
+            }
+        }
+    }
+    //city data
+    alert(city.short_name + " " + city.long_name)
+
+
+    } else {
+      alert("No results found");
+    }
+  } else {
+    alert("Geocoder failed due to: " + status);
+  }
+});
+}
+
