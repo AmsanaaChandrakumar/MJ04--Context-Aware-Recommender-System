b3f9f1b00797dab2204ac9a69c17590bfb4d6754
Jas Lamba <jaskiran.lamba@gmail.com>
2020-01-31 16:52:32 -0500

    formatting js file

--- contextAwareSystem/static/location.js
+++ contextAwareSystem/static/location.js
@@ -23,35 +23,35 @@ function initialize() {
 function codeLatLng(lat, lng) {
 
 var latlng = new google.maps.LatLng(lat, lng);
-geocoder.geocode({'latLng': latlng}, function(results, status) {
-  if (status == google.maps.GeocoderStatus.OK) {
-  console.log(results)
-    if (results[1]) {
-     //formatted address
-     // alert(results[0].formatted_address)
-    //find country name
-         for (var i=0; i<results[0].address_components.length; i++) {
-        for (var b=0;b<results[0].address_components[i].types.length;b++) {
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+    console.log(results)
+      if (results[1]) {
+       //formatted address
+       // alert(results[0].formatted_address)
+      //find country name
+           for (var i=0; i<results[0].address_components.length; i++) {
+          for (var b=0;b<results[0].address_components[i].types.length;b++) {
 
-        //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-            if (results[0].address_components[i].types[b] == "locality") {
-                //this is the object you are looking for
-                city= results[0].address_components[i];
-                break;
-            }
-        }
-    }
-    //city data
-    // alert(city.short_name + " " + city.long_name)
-    document.getElementById("location").innerHTML = "Trending in " + city.long_name;
+          //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+              if (results[0].address_components[i].types[b] == "locality") {
+                  //this is the object you are looking for
+                  city= results[0].address_components[i];
+                  break;
+              }
+          }
+      }
+      //city data
+      // alert(city.short_name + " " + city.long_name)
+      document.getElementById("location").innerHTML = "Trending in " + city.long_name;
 
 
+      } else {
+        alert("No results found");
+      }
     } else {
-      alert("No results found");
+      alert("Geocoder failed due to: " + status);
     }
-  } else {
-    alert("Geocoder failed due to: " + status);
-  }
-});
+  });
 }
 
