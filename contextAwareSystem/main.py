7aadb27740b710aa118f34ca4f696950e78d535e
Jas Lamba <jaskiran.lamba@gmail.com>
2020-01-31 19:55:58 -0500

    setting up infrastructure to send data between apis

--- contextAwareSystem/main.py
+++ contextAwareSystem/main.py
@@ -1,13 +1,27 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+
     return render_template("index.html")
 
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+	# POST request: goes from broweser to flask
+    if request.method == 'POST':
+    	print('Incoming..')
+    	jsonData = request.get_json(force=True)
+        print(jsonData.get('greeting'))  # parse as JSON
+        return str(jsonData.get('greeting')), 200
+
+    # GET request: goes from flask to browser
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8080, debug=True)
