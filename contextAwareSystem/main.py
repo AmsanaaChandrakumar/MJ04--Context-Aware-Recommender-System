825c748d349fd836b7778f8ec3863a2e9c77d8b3
Jas Lamba <jaskiran.lamba@gmail.com>
2020-04-01 17:45:40 -0400

    sending python algorithm to javascript

--- contextAwareSystem/main.py
+++ contextAwareSystem/main.py
@@ -49,44 +49,142 @@ def getWeather():
         jsonData = request.get_json(force=True)
         temperature = int(jsonData.get('temperature'))
         precipitation = str(jsonData.get('precipitation'))
-
-        if temperature <= 0 and precipitation == 'clear sky' or precipitation == 'few clouds' or precipitation == 'scattered clouds' or precipitation == 'broken clouds':
+        if temperature <= 0 and (precipitation == "Clear" or precipitation == "Clouds"):
             weatherLabel = "colddry"
-        elif temperature <= 0 and precipitation == 'rain' or precipitation == 'shower rain' or precipitation == 'thunderstorm' or precipitation == 'mist':
+        elif temperature <= 0 and (precipitation == "Rain" or precipitation == "Drizzle" or precipitation == "Thunderstorm" or precipitation == "Mist" or precipitation == "Fog"):
             weatherLabel = "coldrain"
-        elif temperature <= 0 and precipitation == 'snow':
+        elif temperature <= 0 and precipitation == "Snow":
             weatherLabel = "coldsnow"
-        elif temperature > 0 and temperature <10 and precipitation == 'clear sky' or precipitation == 'few clouds' or precipitation == 'scattered clouds' or precipitation == 'broken clouds' or precipitation == 'overcast clouds':
+        elif temperature > 0 and temperature <10 and (precipitation == "Clear" or precipitation == "Clouds"):
             weatherLabel = "chillydry"
-        elif temperature > 0 and temperature <10 and precipitation == 'rain' or precipitation == 'shower rain' or precipitation == 'thunderstorm' or precipitation == 'mist' or precipitation == 'light intensity drizzle':
+        elif temperature > 0 and temperature <10 and (precipitation == "Rain" or precipitation == "Drizzle" or precipitation == "Thunderstorm" or precipitation == "Mist" or precipitation == "Fog"):
             weatherLabel = "chillyrain"
-        elif temperature > 0 and temperature <10 and precipitation == 'snow':
+        elif temperature > 0 and temperature <10 and precipitation == "Snow":
             weatherLabel = "chillysnow"
-        elif temperature >=10 and temperature <20 and precipitation == 'clear sky' or precipitation == 'few clouds' or precipitation == 'scattered clouds' or precipitation == 'broken clouds':
+        elif temperature >=10 and temperature <20 and (precipitation == "Clear" or precipitation == "Clouds"):
             weatherLabel = "warmdry"
-        elif temperature >=10 and temperature <20 and precipitation == 'rain' or precipitation == 'shower rain' or precipitation == 'thunderstorm' or precipitation == 'mist':
+        elif temperature >=10 and temperature <20 and (precipitation == "Rain" or precipitation == "Drizzle" or precipitation == "Thunderstorm" or precipitation == "Mist" or precipitation == "Fog"):
             weatherLabel = "warmrain"
-        elif temperature >=20 and precipitation == 'clear sky' or precipitation == 'few clouds' or precipitation == 'scattered clouds' or precipitation == 'broken clouds':
+        elif temperature >20 and (precipitation == "Clear" or precipitation == "Clouds"):
             weatherLabel = "hotdry"
         else: 
             weatherLabel = "warmrain"
 
-        sortclothes(weatherLabel)
-        paulsMainFunction()
-        return weatherLabel
+        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+        json_url = os.path.join(SITE_ROOT, "nordstrom.json")
+        nordsdata = json.load(open(json_url))
 
-def sortclothes(weatherLabel):
-    print (weatherLabel)
-    return ""    
+        df = pd.DataFrame(nordsdata[0]['clothing'])
 
-def paulsMainFunction():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "nordstrom.json")
-    nordsdata = json.load(open(json_url))
+        No_Reviews = df['reviews']
+        Rating = df['rating']
 
-    df = pd.DataFrame(nordsdata[0]['clothing'])
-    print (df)
-    return ""
+        df['weighted_rating'] = (0.5*Rating) + (5*(1-0.5))*(1-(np.exp((-No_Reviews)/50)))
+
+        weighted_rating = df['weighted_rating']
+        df['discount_percent'] = pd.to_numeric(df['discount_percent'],errors='coerce')
+        discount_percent = df['discount_percent']
+
+        p = 0.7
+        df['weighted_discount'] = (p*(weighted_rating)) + (5*(1-p))*(1-(np.exp(((-discount_percent)/20))))
+
+        df = df.sort_values(by = ['weighted_discount', 'price'], ascending = [False, True])
+        df = df.drop_duplicates(subset = ['name'])
+        df = df[~df.name.str.contains("nursing|maternity", case = False)]
+
+        colddry = df[df.category.str.contains('hoodie| sweaters|jeans|pants|blazers|coats',case=False)]
+        coldrain = df[df.category.str.contains('blazers|coats|pants |jeans|sweaters|hoodies',case=False)]
+        coldsnow= df[df.category.str.contains('blazers|coats|pants |jeans|sweaters|hoodies',case=False)]
+        chillydry= df[df.category.str.contains('blazers|coats|pants |jeans|sweaters|hoodies',case=False)]
+        chillyrain= df[df.category.str.contains('blazers|coats|pants |jeans|sweaters|hoodies',case=False)]
+        chillysnow= df[df.category.str.contains('blazers|coats|pants |jeans|sweaters|hoodies',case=False)]
+        warmdry= df[df.category.str.contains('shorts|polo|jeans| hoodie| t-shirt| dress |romper|top|sweater',case=False)]
+        warmrain = df[df.category.str.contains('shorts|polo|jeans| hoodie| t-shirt|dress|romper|top|sweater ',case=False)]
+        hotdry= df[df.category.str.contains('shorts|polo|jeans|tank|shirts|dress|romper|top|swimsuit',case=False)]
+        hotrain= df[df.category.str.contains('shorts|polo|jeans|tank|shirts|dress|romper|top|swimsuit',case=False)]
+
+        if weatherLabel == "colddry": 
+            colddrymale = colddry[colddry['gender'].isin(['MALE'])]
+            colddrymale = colddrymale.drop_duplicates(subset = ['category'])
+            colddryfemale = (colddry[colddry['gender'].isin(['FEMALE'])])
+            colddryfemale = colddryfemale.drop_duplicates(subset = ['category'])
+            sort_colddry = pd.concat([colddrymale.head(3), colddryfemale.head(3)])
+
+            return sort_colddry.to_json(orient='records')
+        elif weatherLabel == "coldrain":
+            coldrainmale = coldrain[coldrain['gender'].isin(['MALE'])]
+            coldrainmale = coldrainmale.drop_duplicates(subset = ['category'])
+            coldrainfemale = (coldrain[coldrain['gender'].isin(['FEMALE'])])
+            coldrainfemale = coldrainfemale.drop_duplicates(subset = ['category'])
+            sort_coldrain = pd.concat([coldrainmale.head(3), coldrainfemale.head(3)])
+
+            return sort_coldrain.to_json(orient='records')
+        elif weatherLabel == "coldsnow":
+            coldsnowmale = coldsnow[coldsnow['gender'].isin(['MALE'])]
+            coldsnowmale = coldsnowmale.drop_duplicates(subset = ['category'])
+            coldsnowfemale = (coldsnow[coldsnow['gender'].isin(['FEMALE'])])
+            coldsnowfemale = coldsnowfemale.drop_duplicates(subset = ['category'])
+            sort_coldsnow = pd.concat([coldsnowmale.head(3), coldsnowfemale.head(3)])
+
+            return sort_coldsnow.to_json(orient='records')
+        elif weatherLabel == "chillydry":
+            chillydrymale = chillydry[chillydry['gender'].isin(['MALE'])]
+            chillydrymale = chillydrymale.drop_duplicates(subset = ['category'])
+            chillydryfemale = (chillydry[chillydry['gender'].isin(['FEMALE'])])
+            chillydryfemale = chillydryfemale.drop_duplicates(subset = ['category'])
+            sort_chillydry = pd.concat([chillydrymale.head(3), chillydryfemale.head(3)])
+
+            return sort_chillydry.to_json(orient='records')
+        elif weatherLabel == "chillyrain":
+            chillyrainmale = chillyrain[chillyrain['gender'].isin(['MALE'])]
+            chillyrainmale = chillyrainmale.drop_duplicates(subset = ['category'])
+            chillyrainfemale = (chillyrain[chillyrain['gender'].isin(['FEMALE'])])
+            chillyrainfemale = chillyrainfemale.drop_duplicates(subset = ['category'])
+            sort_chillyrain = pd.concat([chillyrainmale.head(3), chillyrainfemale.head(3)])
+
+            return sort_chillyrain.to_json(orient='records')
+        elif weatherLabel == "chillysnow":
+            chillysnowmale = chillysnow[chillysnow['gender'].isin(['MALE'])]
+            chillysnowmale = chillysnowmale.drop_duplicates(subset = ['category'])
+            chillysnowfemale = (chillysnow[chillysnow['gender'].isin(['FEMALE'])])
+            chillysnowfemale = chillysnowfemale.drop_duplicates(subset = ['category'])
+            sort_chillysnow = pd.concat([chillysnowmale.head(3), chillysnowfemale.head(3)])
+
+            return sort_chillysnow.to_json(orient='records')
+        elif weatherLabel == "warmdry":
+            warmdrymale = warmdry[warmdry['gender'].isin(['MALE'])]
+            warmdrymale = warmdrymale.drop_duplicates(subset = ['category'])
+            warmdryfemale = (warmdry[warmdry['gender'].isin(['FEMALE'])])
+            warmdryfemale = warmdryfemale.drop_duplicates(subset = ['category'])
+            sort_warmdry = pd.concat([warmdrymale.head(3), warmdryfemale.head(3)])
+
+            return sort_warmdry.to_json(orient='records')
+        elif weatherLabel == "warmrain":
+            warmrainmale = warmrain[warmrain['gender'].isin(['MALE'])]
+            warmrainmale = warmrainmale.drop_duplicates(subset = ['category'])
+            warmrainfemale = (warmrain[warmrain['gender'].isin(['FEMALE'])])
+            warmrainfemale = warmrainfemale.drop_duplicates(subset = ['category'])
+            sort_warmrain = pd.concat([warmrainmale.head(3), warmrainfemale.head(3)])
+
+            return sort_warmrain.to_json(orient='records')
+        elif weatherLabel == "hotdry":
+            hotdrymale = hotdry[hotdry['gender'].isin(['MALE'])]
+            hotdrymale = hotdrymale.drop_duplicates(subset = ['category'])
+            hotdryfemale = (hotdry[hotdry['gender'].isin(['FEMALE'])])
+            hotdryfemale = hotdryfemale.drop_duplicates(subset = ['category'])
+            sort_hotdry = pd.concat([hotdrymale.head(3), hotdryfemale.head(3)])
+
+            return sort_hotdry.to_json(orient='records')
+        elif weatherLabel == "hotrain":
+            hotrainmale = hotrain[hotrain['gender'].isin(['MALE'])]
+            hotrainmale = hotrainmale.drop_duplicates(subset = ['category'])
+            hotrainfemale = (hotrain[hotrain['gender'].isin(['FEMALE'])])
+            hotrainfemale = hotrainfemale.drop_duplicates(subset = ['category'])
+            sort_hotrain = pd.concat([hotrainmale.head(3), hotrainfemale.head(3)])
+
+            return sort_hotrain.to_json(orient='records')
+
+
 # @app.route('/googleMaps', methods=['GET', 'POST'])
 # def googleMaps():
 #     if request.method == 'POST':
